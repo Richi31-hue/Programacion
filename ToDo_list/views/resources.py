e12e0392afcb874b1bfa78fb9fe606349e368c8d
Richi31-hue <utm24090336@utma.edu.mx>
2025-07-10 20:34:41 -0600

    Mini_proyecto

---
+++ ToDo_list/views/resources.py
@@ -0,0 +1,12 @@
+TITLE = ("Arial",25)
+SUBTITLE = ("Arial",20)
+TEXT = ("Arial",15)
+
+
+
+ICON = "ToDo_list/views/img/Actividad.ico"
+LOGO = "ToDo_list/views/img/ACTIVIDADES.gif"
+PRIMARY_COLOR = "#4C1E4F"
+SECONDARY_COLOR = "#FEE1C7" 
+THIRD_COLOR = "#FCB094" 
+FOURTH_COLOR = "#F44174" 
