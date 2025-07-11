1ceb985157657ed1451e4fdad7ee594459f4524c
Richi31-hue <utm24090336@utma.edu.mx>
2025-07-10 21:51:08 -0600

    Ahora si

--- ToDo_list/views/resources.py
+++ ToDo_list/views/resources.py
@@ -4,8 +4,8 @@ TEXT = ("Arial",15)
 
 
 
-ICON = "ToDo_list/views/img/Actividad.ico"
-LOGO = "ToDo_list/views/img/ACTIVIDADES.gif"
+ICON = "ToDo_list/views/img/ACTIVIDADES.gif"
+LOGO = "ToDo_list/views/img/Actividad.ico"
 PRIMARY_COLOR = "#4C1E4F"
 SECONDARY_COLOR = "#FEE1C7" 
 THIRD_COLOR = "#FCB094" 
